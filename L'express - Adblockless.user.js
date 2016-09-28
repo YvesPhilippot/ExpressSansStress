936ef3619cdf47dd8cd99e5b1da91c1d20b75126
YvesPhilippot <yvessnake@hotmail.com>
2016-09-28 14:10:52 +0200

    Tampermonkey pour masquer le message AdBlock

---
+++ L'express - Adblockless.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         L'express - Adblockless
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  Afficher les news abonnés Le Monde lorsqu'on est en lecture Zen
+// @author       El_Rhonin
+// @match        http://www.lexpress.fr/*
+// @grant        GM_addStyle
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// ==/UserScript==
+/* jshint -W097 */
+// Your code here...
+$(document).ready(function() {
+    
+
+      
+     $(".gam_info").hide();
+
+    
+});
